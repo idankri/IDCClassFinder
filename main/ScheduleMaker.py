b996bb99a5d043fb9e4b8115319a824027a9f152
idankri <58448747+idankri@users.noreply.github.com>
2020-03-15 09:41:13 +0200

    General code improvement

--- main/ScheduleMaker.py
+++ main/ScheduleMaker.py
@@ -1,5 +1,5 @@
 """
-@creator: Idan Kringel
+@author: Idan Kringel
 Updated to IDC website as of March 2020
 Written using python 3.7
 requirements:
@@ -21,6 +21,16 @@ import time, re
 
 CHROME_DRIVER = r'C:\Webdrivers\chromedriver.exe'
 
+YEDION_URL = r'http://portal.idc.ac.il/he/main/services/newsletter_regulations/yedion/pages/thisyearheb.aspx'
+
+SCHOOL_XPATH = r'//*[@id="ctl00_ctl26_g_cdf367b8_9960_4a77_9e5b_247c7c6d5387_ctl00_CurriculumPickerSchedule_School"]'
+YEAR_XPATH = r'//*[@id="ctl00_ctl26_g_cdf367b8_9960_4a77_9e5b_247c7c6d5387_ctl00_CurriculumPickerSchedule_StudyYear"]'
+SPEC_XPATH = r'//*[@id="ctl00_ctl26_g_cdf367b8_9960_4a77_9e5b_247c7c6d5387_ctl00_CurriculumPickerSchedule_Specialization"]'
+SEARCH_XPATH = r'//*[@id="ctl00_ctl26_g_cdf367b8_9960_4a77_9e5b_247c7c6d5387_ctl00_CurriculumPickerSchedule_CurriculumSearch"]'
+
+FIRST_SEMESTER_LINK_ID = u'ctl00_ctl26_g_cdf367b8_9960_4a77_9e5b_247c7c6d5387_ctl00_ScheduleResults_ScheduleSemesterLinks_ctl01_ShowSchedules'
+SECOND_SEMESTER_LINK_ID = u'ctl00_ctl26_g_cdf367b8_9960_4a77_9e5b_247c7c6d5387_ctl00_ScheduleResults_ScheduleSemesterLinks_ctl03_ShowSchedules'
+
 
 class ScheduleMaker:
     def __init__(self):
@@ -33,7 +43,7 @@ class ScheduleMaker:
         :return: a selenium chrome driver
         """
         driver = webdriver.Chrome(CHROME_DRIVER)
-        driver.get(r'http://portal.idc.ac.il/he/main/services/newsletter_regulations/yedion/pages/thisyearheb.aspx')
+        driver.get(YEDION_URL)
         driver.find_elements_by_link_text(u'מערכת שעות')[0].click()
         return driver
 
@@ -41,43 +51,24 @@ class ScheduleMaker:
         """
         A generator, in each iteration makes driver goto next table and then yields
         :param semester: 1 for first Semester else 2
-        TODO: Make less ugly :)
         """
-        school_num = 2  # temp value
-        current_school = 1
-        while current_school < school_num:
-            se = Select(self.driver.find_elements_by_name(
-                u'ctl00$ctl26$g_cdf367b8_9960_4a77_9e5b_247c7c6d5387$ctl00$CurriculumPickerSchedule$School')[0])
-            school_num = len(se.options)
-            se.select_by_visible_text(se.options[current_school].text)
-            # time.sleep(0.1)
+        school_num = DriverUtils.get_option_num(self.driver, SCHOOL_XPATH)
+        for current_school in range(1, school_num):
+            DriverUtils.select_option(self.driver, SCHOOL_XPATH, current_school)
 
-            current_year = 1
-            year_num = 2  # temp value
-            while current_year < year_num:
-                se = Select(self.driver.find_elements_by_name(
-                    u'ctl00$ctl26$g_cdf367b8_9960_4a77_9e5b_247c7c6d5387$ctl00$CurriculumPickerSchedule$StudyYear')[0])
-                year_num = len(se.options)
-                se.select_by_visible_text(se.options[current_year].text)
-                # time.sleep(0.1)
+            year_num = DriverUtils.get_option_num(self.driver, YEAR_XPATH)
+            for current_year in range(1, year_num):
+                DriverUtils.select_option(self.driver, YEAR_XPATH, current_year)
 
-                current_spec = 1
-                specs_num = 2  # temp value
-                while current_spec < specs_num:
-                    se = Select(self.driver.find_elements_by_name(
-                        u'ctl00$ctl26$g_cdf367b8_9960_4a77_9e5b_247c7c6d5387$ctl00$CurriculumPickerSchedule$Specialization'
-                    )[0])
-                    specs_num = len(se.options)
-                    se.select_by_visible_text(se.options[current_spec].text)
-                    time.sleep(0.1)
-                    self.driver.find_elements_by_name(
-                        u'ctl00$ctl26$g_cdf367b8_9960_4a77_9e5b_247c7c6d5387$ctl00$CurriculumPickerSchedule$CurriculumSearch')[
-                        0].click()
-                    time.sleep(1)
+                specs_num = DriverUtils.get_option_num(self.driver, YEAR_XPATH)
+                for current_spec in range(1, specs_num):
+                    DriverUtils.select_option(self.driver, SPEC_XPATH, current_spec)
+
+                    self.driver.find_elements_by_xpath(SEARCH_XPATH)[0].click()
+                    time.sleep(0.5)
                     try:
-                        table_ext_id = u'1' if semester == 1 else u'3'
-                        table_id_str = u'ctl00_ctl26_g_cdf367b8_9960_4a77_9e5b_247c7c6d5387_ctl00_ScheduleResults_ScheduleSemesterLinks_ctl0' + table_ext_id + u'_ShowSchedules'
-                        self.driver.find_element_by_id(table_id_str).click()
+                        sem_id = FIRST_SEMESTER_LINK_ID if semester == 1 else SECOND_SEMESTER_LINK_ID
+                        self.driver.find_element_by_id(sem_id).click()
                         yield
                     except selenium.common.exceptions.NoSuchElementException:
                         pass
@@ -129,12 +120,25 @@ class ScheduleMaker:
         :param day: 1 for sunday, 6 for friday, etc..
         :return: the class dict
         """
-        reg = re.compile(r"(?P<time>\d{1,2}:\d{2} - \d{1,2}:\d{2})\n[^a-zA-Z0-9]*(?P<class>[a-zA-Z0-9\.]+)")
+        reg = re.compile(
+            r"(?P<time_frame>\d{1,2}:\d{2} - \d{1,2}:\d{2})\n[^a-zA-Z0-9]*(?P<class_room>[a-zA-Z0-9\.]+)")
         all_times = reg.findall(text)
-        for value, key in all_times:
-            if value not in class_dict[key][day]:
-                class_dict[key][day].append(value)
+        for time_frame, class_room in all_times:
+            if time_frame not in class_dict[class_room][day]:
+                class_dict[class_room][day].append(time_frame)
         return class_dict
+
+
+class DriverUtils:
+    @staticmethod
+    def get_option_num(driver, xpath):
+        se = Select(driver.find_element_by_xpath(xpath))
+        return len(se.options)
+
+    @staticmethod
+    def select_option(driver, xpath: str, option: int):
+        se = Select(driver.find_element_by_xpath(xpath))
+        se.select_by_visible_text(se.options[option].text)
 
 
 if __name__ == '__main__':
